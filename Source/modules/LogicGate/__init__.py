d1673f0e4b7def7e435caddd0c188524f0ac3da4
Moon-minor <dibochh@gmail.com>
2026-09-07 22:16:51 +0800

    Update LG mod

--- Source/modules/LogicGate/__init__.py
+++ Source/modules/LogicGate/__init__.py
@@ -6,4 +6,21 @@ def and_logic(self):
     b = self.IOs["B"].GetInput()
     # set ouput
     self.IOs["OUT"].Output(a and b)
-    return CircuitObject("AND_Gate", {"A": CircuitIO("A"), "B": CircuitIO("B"), "OUT": CircuitIO("OUT")}, and_logic)
+
+def or_logic(self):
+    a = self.IOs["A"].GetInput()
+    b = self.IOs["B"].GetInput()
+    self.IOs["OUT"].Output(a or b)
+
+def not_logic(self):
+    a = self.IOs["A"].GetInput()
+    self.IOs["OUT"].Output(not a)
+
+
+
+def ANDGate(name):
+    return CircuitObject(name, {"A": CircuitIO("A"), "B": CircuitIO("B"), "OUT": CircuitIO("OUT")}, and_logic)
+def ORGate(name):
+    return CircuitObject(name, {"A": CircuitIO("A"), "B": CircuitIO("B"), "OUT": CircuitIO("OUT")}, or_logic)
+def NOTGate(name):
+    return CircuitObject(name, {"A": CircuitIO("A"), "OUT": CircuitIO("OUT")}, not_logic)
