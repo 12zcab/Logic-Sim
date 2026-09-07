a873ce8c6da434c2b27a6db6268f8456447308e2
12zcab <61446775+12zcab@users.noreply.github.com>
2026-09-07 21:50:25 +0800

    Chilam (#4)
    * Create Rock JohnSon
    
    * delete John Cena
    
    * Create Spider Man
    
    * slight fix
    
    * Finished Basic Structure and Wrote the first module
    
    * Fix Typp
    
    ---------
    
    Co-authored-by: Walker <walkerentrepreneur@gmail.com>

--- Source/core/object.py
+++ Source/core/object.py
@@ -6,10 +6,12 @@
 import gc
 
 class CircuitObject:
-    def __init__(self, name, IOs, update_func):
+    def __init__(self, name, IOs, updateFunc):
         self.name = name
         self.IOs = IOs  # Dictionary: {"PinName": CircuitIO}
-        self.update_func = update_func
+        self.updater = updateFunc  # Function that defines the object's behavior
+    def update_func(self):
+        self.updater(self)
 
 
 class CircuitIO:
@@ -65,35 +67,34 @@ class CircuitIO:
 
 class Net:
     def __init__(self, name=None):  # Fixed: name defaults to None
-        self.ios = []
+        self.IOs = []
         self.name = name if name else f"Net_{id(self)}"
 
     def AddIO(self, io):
-        if io not in self.ios:
-            self.ios.append(io)
+        if io not in self.IOs:
+            self.IOs.append(io)
 
     def RemoveIO(self, io):
-        if io in self.ios:
-            self.ios.remove(io)
+        if io in self.IOs:
+            self.IOs.remove(io)
 
     def Clear(self):
-        for io in self.ios:
+        for io in self.IOs:
             io.ValIn = False
 
     def Update(self):
         self.Clear()
-        if any(io.ValOut for io in self.ios):
-            for io in self.ios:
+        if any(io.ValOut for io in self.IOs):
+            for io in self.IOs:
                 io.ValIn = True
 
     def PrepareDelete(self):
-        for io in self.ios:
+        for io in self.IOs:
             io.connected_net = None
-        self.ios.clear()
+        self.IOs.clear()
 
     def MergeNets(self, other_net):
-        # Fixed: iterate over list(...) snapshot to prevent mid-loop deletion bugs
-        for io in list(other_net.ios):
+        for io in list(other_net.IOs):
             self.AddIO(io)
             io.connected_net = self
         DestroyNet(other_net)
@@ -112,10 +113,7 @@ class SimBox:
         self.Nets = Nets
 
     def update(self):
-        # Step 1: Objects evaluate inputs and set outputs
         for obj in self.Objects:
-            obj.update_func(obj.IOs)
-
-        # Step 2: Nets propagate signals across connected pins
+            obj.update_func()
         for net in self.Nets:
             net.Update()
