fed1dcd2fa99135e3f24cce49a0bdc12736366a3
Moon-minor <dibochh@gmail.com>
2026-09-09 09:46:26 +0800

    ADD srlatch mux21 demux12 etc.

--- Source/modules/LogicGate/__init__.py
+++ Source/modules/LogicGate/__init__.py
@@ -37,6 +37,39 @@ def half_adder_logic(self):
     self.IOs["SUM"].Output(a ^ b)      # XOR
     self.IOs["CARRY"].Output(a and b)   # AND
 
+def full_adder_logic(self):
+    a = self.IOs["A"].GetInput()
+    b = self.IOs["B"].GetInput()
+    c = self.IOs["C"].GetInput()
+    self.IOs["SUM"].Output(a ^ b ^ c)   #S = A ⊕ B ⊕ C
+    self.IOs["CARRY"].Output((a and b) or (b and c) or (c and a))   #Carry = AB + BC + AC
+
+def mux21_logic(self):
+    i0 = self.IOs["I0"].GetInput()
+    i1 = self.IOs["I1"].GetInput()
+    s = self.IOs["S"].GetInput()
+    # Out = (NOT S AND D0) OR (S AND D1)
+    self.IOs["OUT"].Output(i1 if s else i0)
+
+def demux12_logic(self):
+    in_val = self.IOs["IN"].GetInput()
+    s = self.IOs["S"].GetInput()
+    self.IOs["Y0"].Output(in_val if not s else False)
+    self.IOs["Y1"].Output(in_val if s else False)
+
+def sr_latch_logic(self):
+    # initial
+    if not hasattr(self, "state"):
+        self.state = False
+    s = self.IOs["S"].GetInput()
+    r = self.IOs["R"].GetInput()
+    if r:
+        self.state = s
+        
+    self.IOs["Q"].Output(self.state)
+    self.IOs["QN"].Output(not self.state)
+
+
 def ANDGate(name):
     return CircuitObject(name, {"A": CircuitIO("A"), "B": CircuitIO("B"), "OUT": CircuitIO("OUT")}, and_logic)
 def NANDGate(name):
@@ -51,8 +84,36 @@ def NOTGate(name):
     return CircuitObject(name, {"A": CircuitIO("A"), "OUT": CircuitIO("OUT")}, not_logic)
 def half_adder(name):
     return CircuitObject(name,{
-        "A": CircuitIO(f"{name}_A"),
-        "B": CircuitIO(f"{name}_B"),
-        "SUM": CircuitIO(f"{name}_SUM"),
-        "CARRY": CircuitIO(f"{name}_CARRY")
+        "A": CircuitIO("A"),
+        "B": CircuitIO("B"),
+        "SUM": CircuitIO("SUM"),
+        "CARRY": CircuitIO("CARRY")
     }, half_adder_logic)
+def full_adder(name):
+    return CircuitObject(name,{
+        "A": CircuitIO("A"),
+        "B": CircuitIO("B"),
+        "SUM": CircuitIO("SUM"),
+        "CARRY": CircuitIO("CARRY")
+    }, full_adder_logic)
+def mux21(name):
+    return CircuitObject(name,{
+        "I0": CircuitIO("I0"),
+        "I1": CircuitIO("I1"),
+        "S": CircuitIO("S"),
+        "OUT": CircuitIO("OUT")
+    }, mux21_logic)
+def demux21(name):
+    return CircuitObject(name,{
+        "IN": CircuitIO("IN"),
+        "S": CircuitIO("S"),
+        "Y0": CircuitIO("Y0"),
+        "Y1": CircuitIO("Y1")
+    }, demux12_logic)
+def sr_latch(name):
+    return CircuitObject(name,{
+            "S": CircuitIO("S"),
+            "R": CircuitIO("R"),
+            "Q": CircuitIO("Q"),
+            "QN": CircuitIO("QN")
+        }, sr_latch_logic)
