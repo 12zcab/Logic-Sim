4cacae701a68c0c17756cf3ac02aab1edc6c70d3
12zcab <61446775+12zcab@users.noreply.github.com>
2026-09-11 20:44:38 +0800

    New changes (#10)
    * New changes
    
    * new things
    
    ---------
    
    Co-authored-by: Walker <walkerentrepreneur@gmail.com>

--- Source/core/Data/DataHandler(ver1).py
+++ Source/core/Data/DataHandler(ver1).py
@@ -3,47 +3,95 @@ import json
 import os
 
 class Store:
-    def __init__(self, tableName, **kwargs):
+    def __init__(self, tableName, filename, **kwargs):
         self.data = kwargs
+        self.Filename = filename
         self.tableName = tableName
 
-    def DataProcessing(self):
-        try:
-            with open(self.filename, "r") as file:
-                self.File = json.load(file)
-                try:
-                    if self.table == self.File.get(self.tableName):
-                        print("Found")
-                except self.table == False:
-                    self.table = {}
-
-        except FileNotFoundError or json.JSONDecodeError:
-            self.File = open(self.filename, "x").close()
-            self.table = {}
 
     def DataStore(self):
-        for key, values in self.data.items():
-            self.table[key] = values
+        self.File = Loader.FileSearcher(self.Filename)
+        if self.File == False:
+            ConditionHandle.FileNotFound(self.File, self.tableName)
 
-
-        with open(self.filename, "w") as file:
-            json.dump(self.File, file, indent=4)
-
+        
 
 
 
-class Retreiver: 
-    def __init__(self, table, key):
-        self.table = table
-        self.key = key
+class ConditionHandle:
 
-    def RetreiveProcessing(self):
-        self.file = 
+    def Checking(self):
+        return
+        
+    def FileNotFound(self):
+        return
+
+    def TableNotFound(self):
+        return
+        
+
+
+class Loader:    
+
+    def FileSearcher(Filename):
+        try:
+            with open(Filename, "x") as file:
+                return file
+
+        except FileNotFoundError or json.JSONDecodeError:
+            return False
+
+    def TableSearcher(File, Table):
+        try:
+            if File.get(Table):
+                return File.get(Table)
+        except not File.get(Table):
+            return False
+
+
+
+    def ValueSearcher(Table, Keys):
+
+        TotalData = {}
+
+        try:
+            for key1, value1 in Table:
+                for key2, _ in Keys:
+                    if key1 == key2:
+                        TotalData[key2] = value1
+                    else
+                        
+
+        except
+
+
+
+
 
     
 
+        
+
+
+
+
+ 
+
+class Retreiver: 
+    def __init__(self, table, key, filename):
+        self.table = table
+        self.key = key
+        self.filename = filename
+
+    def WorkFlower(self):
+        return
+    
+
+
+    
 
     def DataRetrieve(self):
+        return
 
 
     
