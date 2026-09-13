e15d975ec656b8a43c32ae8fdc1bdfdd5ed415a7
Walker <walkerentrepreneur@gmail.com>
2026-09-13 15:51:13 +0800

    Export

--- Source/Temp/Modules/Input + output/Export.py
+++ Source/Temp/Modules/Input + output/Export.py
@@ -6,15 +6,44 @@ import json
 
  
 class Exporter:
-    def __init__(self, items, filetype):
+    def __init__(self, items, filename):
         self.items = items
-        self.filetype = filetype
+        self.filename = filename
  
-    def Export(self, Simbox):
+    def ExportObject(self, Simbox):
         SimBoxData = {
             "Conponets" : [],
             "Nets" : []
         }
+        
+        try:
+            with open(self.filename, "x") as file:
+                self.file = file
+        except FileNotFoundError:
+            self.file = os.makedirs(self.filename, exist_ok=True)
+
+
+        for key1, value1 in (SimBox.Objects):
+            SimBoxData["Conponets"][key1] = value1
+
+        for key2, value2 in (SimBox.Nets):
+            SimBoxData["Nets"][key2] = value2
+
+        json.dump(SimBoxData, self.file, indent=4)
+    
+
+
+
+        
+        
+        
+            
+
+        
+
+
+
+
         
         
 
